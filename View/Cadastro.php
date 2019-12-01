540cd6936cb7fa1dd3bfbee492d3cb19f28f9cc8
Laércio Marques <laerciom99@gmail.com>
2019-12-01 14:48:24 -0300

    Login e cadastro css/html

--- View/Cadastro.php
+++ View/Cadastro.php
@@ -2,26 +2,32 @@
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+         <link rel="stylesheet" type="text/css" href="cad.css">
+        <title>RPG</title>
     </head>
     <body>
     <center>
+        <div id="cadastro">
         <form method="POST" action="../Controller/validar-cadastro.php">
-            Seu nome: <input type="text" name="nome" placeholder="ex. Anderson" required="">*<br>
-            E-mail: <input type="email" name="email" placeholder="exemplo@exemplo.com" required="">*<br>
-            Informações: <textarea name="info" cols="30" rows="10"></textarea>
-            Jogo favorito: <select name="Jogos" id="jogo">
+            <h1>Cadastro</h1><p></p><br>
+            <h4> Nome</h4> <input type="text" name="nome" placeholder="Nick" required=""><p></p>
+           <h4> E-mail</h4> <input type="email" name="email" placeholder="@hotmail.com" required="">
+           <h4> Informações</h4> <textarea name="info" cols="10" rows="001"></textarea> <p></p>
+              Jogo favorito: <select name="Jogos" id="jogo">
                 <option value="jogo 1">jogo 1</option>
                 <option value="jogo 2">jogo 2</option>
                 <option value="jogo 3">jogo 3</option>
                 <option value="jogo 4">jogo 4</option>
             </select>
-            Cpf: <input type="number" name="cpf" placeholder="ex: xxx.xxx.xxx-xx" required="">*<br>
+              <p></p>
+            <h4>  CPF: </h4> <input type="password" name="cpf" placeholder="000.000.000-00" required=""><p></p>
             <input type="submit" value="Cadastrar">
+            <p></p>
         </form>
         <form method="POST" action="../View/index.php">
-            Já tem conta? <input type="submit" value="Clique aqui!">
+            <h4> Possui conta? </h4> <input type="submit" value="Entre!">
         </form>
+        </div>
     </center>
     </body>
 </html>
--- View/index.php
+++ View/index.php
@@ -2,27 +2,37 @@
 
 
 //Limpeza e destruição da sessão
-  session_start();
-  session_unset();
-  session_destroy();
+session_start();
+session_unset();
+session_destroy();
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>RPG</title>
     </head>
     <body>
     <center>
+        <div id="login">
         <form method="POST" action="../Controller/validar-login.php">
-            <input type="text" name="login" placeholder="Login" required=""><br>
-            <input type="password" name="cpf" placeholder="Cpf" required=""><br>
+            <br><br><br> <h1>Login</h1><br><br><br><br>
+            <h4>Usuário</h4>
+            <p></p>
+            <input type="text" name="login" placeholder="Nick" required=""><p></p>
+            <h4>CPF</h4>
+            <p></p>
+            <input type="password" name="cpf" placeholder="000.000.000-00" required=""><br><p></p>
+            <br><br>
             <input type="submit" value="Login">    
         </form>
         <form method="POST" action="Cadastro.php">
+            <p></p>
             <input type="submit" value="Cadastrar">
         </form>
     </center>
+    </div>
     </body>
 </html>
