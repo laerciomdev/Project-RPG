566e62a51ede7c5cf451850b3d32bdc5d01b7165
Laércio Marques <laerciom99@gmail.com>
2019-12-01 15:11:58 -0300

    Banco e corrigindo feed

--- Controller/postFeed.php
+++ Controller/postFeed.php
@@ -1,38 +1,36 @@
 <?php
+
 session_start();
-
 include_once '../Model/conexao.php';
-
-//Verificar se o usuario clicou no botao, clicou no botao acessa o IF e tenta cadastrar, caso contrario acessa o ELSE
-$recebearquivo = filter_input(INPUT_POST, 'arquivo', FILTER_SANITIZE_STRING);
-$comentario = filter_input(INPUT_POST, 'comentario', FILTER_SANITIZE_STRING);
-
-if ($recebearquivo && $comentario) {
-    //Receber os dados do formulario    
-    $nome_arquivo = $_FILES['arquivo']['name'];
+//Verificar se o usuário clicou no botão, clicou no botão acessa o IF e tenta cadastrar, caso contrario acessa o ELSE
+$SendCadImg = filter_input(INPUT_POST, 'SendCadImg', FILTER_SANITIZE_STRING);
+if ($SendCadImg) {
+    //Receber os dados do formulário
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+    $nome_imagem = $_FILES['imagem']['name'];
     //var_dump($_FILES['imagem']);
     //Inserir no BD
-    $insere_arquivo = "INSERT INTO arquivos (comentario, arquivo) VALUES (:comentario, :imagem)";
-    $insert_msg = $conn->prepare($insere_arquivo);
-    $insert_msg->bindParam(':comentario', $comentario);
-    $insert_msg->bindParam(':arquivo', $arquivo);
+    $result_img = "INSERT INTO imagens (nome, imagem) VALUES (:nome, :imagem)";
+    $insert_msg = $conn->prepare($result_img);
+    $insert_msg->bindParam(':nome', $nome);
+    $insert_msg->bindParam(':imagem', $nome_imagem);
 
     //Verificar se os dados foram inseridos com sucesso
     if ($insert_msg->execute()) {
-        //Recuperar ultimo ID inserido no banco de dados
+        //Recuperar último ID inserido no banco de dados
         $ultimo_id = $conn->lastInsertId();
 
-        //Diretorio onde o arquivo vai ser salvo
-        $diretorio = '../View/imagens/' . $ultimo_id.'/';
+        //Diretório onde o arquivo vai ser salvo
+        $diretorio = 'imagens/' . $ultimo_id.'/';
 
         //Criar a pasta de foto 
         mkdir($diretorio, 0755);
         
-        if(move_uploaded_file($_FILES['arquivo']['tmp_arquivo'], $diretorio.$arquivo)){
-            $_SESSION['msg'] = "<p style='color:green;'>Dados salvo com sucesso</p>";
+        if(move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio.$nome_imagem)){
+            $_SESSION['msg'] = "<p style='color:green;'>Dados salvo com sucesso e upload da imagem realizado com sucesso</p>";
             header("Location: ../View/feed.php");
         }else{
-            $_SESSION['msg'] = "<p><span style='color:green;'>Dados salvo com sucesso. </span><span style='color:red;'>Erro ao realizar o upload do arquivo</span></p>";
+            $_SESSION['msg'] = "<p><span style='color:green;'>Dados salvo com sucesso. </span><span style='color:red;'>Erro ao realizar o upload da imagem</span></p>";
             header("Location: ../View/feed.php");
         }        
     } else {
@@ -41,6 +39,5 @@ if ($recebearquivo && $comentario) {
     }
 } else {
     $_SESSION['msg'] = "<p style='color:red;'>Erro ao salvar os dados</p>";
-    header("Location: ../View/feed.php");
+     header("Location: ../View/feed.php");
 }
-
--- projeto.sql
+++ projeto.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Dez-2019 às 17:12
+-- Tempo de geração: 01-Dez-2019 às 19:11
 -- Versão do servidor: 10.4.6-MariaDB
--- versão do PHP: 7.3.9
+-- versão do PHP: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `imagens`
+--
+
+CREATE TABLE `imagens` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `imagem` varchar(220) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `imagens`
+--
+
+INSERT INTO `imagens` (`id`, `nome`, `imagem`) VALUES
+(1, 'Teste 1', 'cinco.png'),
+(2, 'w', ''),
+(3, 'wwwwwwwww', '001.jpg'),
+(4, '', 'FireShot Capture 003 - preconseito - .png'),
+(5, 'test', 'FireShot Capture 003 - preconseito - .png'),
+(6, 'ttttttttttttt', 'Fenix.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `post`
+--
+
+CREATE TABLE `post` (
+  `idPost` int(11) NOT NULL,
+  `arquivo` varchar(220) DEFAULT NULL,
+  `comentario` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `usuarios`
 --
 
@@ -34,21 +70,26 @@ CREATE TABLE `usuarios` (
   `cpf` varchar(200) NOT NULL,
   `nome` varchar(200) NOT NULL,
   `informacoes` varchar(200) DEFAULT NULL,
-  `jogofavorito` varchar(200) DEFAULT NULL,
-  `diretorio` varchar(250) NOT NULL DEFAULT './imagens/perfil/user.png'
+  `jogofavorito` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `email`, `cpf`, `nome`, `informacoes`, `jogofavorito`, `diretorio`) VALUES
-(5, 'teste@gmail.com', 'celso', 'celso', 'Nenhuma informaÃ§Ã£o...', 'jogo 1', './imagens/perfil/java.png'),
-(12, 'teste@gmail.com', '1', 'joanderson', 'Nenhuma informaÃ§Ã£o...', 'jogo 1', './imagens/perfil/2a1c47b3bb8dbe9cce0b6c76aed2f8e6.jpg');
+INSERT INTO `usuarios` (`id`, `email`, `cpf`, `nome`, `informacoes`, `jogofavorito`) VALUES
+(1, 'carloseduardozero0@gmail.com', '123', 'Carlos', 'eduardo', 'carloseduardobahia@hotmail.com'),
+(2, 'laerciom99@gmail.com', 'laercio', 'laercio', 'gosto de jogos', 'The Legend of Zelda: Ocarina of time');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`idPost`);
 
 --
 -- Índices para tabela `usuarios`
@@ -61,10 +102,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `post`
+--
+ALTER TABLE `post`
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
