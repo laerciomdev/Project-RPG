3b2031e72a5c5a4203e1cee7e5a831120ace7a96
joanderson <joandersonsantos376@gmail.com>
2019-12-01 19:03:29 -0300

    Login corrigido

--- Controller/validar-login.php
+++ Controller/validar-login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 
 require_once "../Model/conexao.php";
 require_once "../Model/Usuario.php";
@@ -18,40 +20,37 @@ $cpf =    $user->getCpf();
 
 
   $query = $con->prepare("SELECT * FROM usuarios WHERE nome = :nome
-   OR email = :email AND cpf = :cpf");
+  OR email = :email");
+  
+  
   $query->bindValue(":nome", $login);
   $query->bindValue(":email", $login);
-  $query->bindValue(":cpf", $cpf);
+
   $query->execute();
-
-
-/* $query->execute(array($login, $login));//Vai substituir os sinais de interrogação pela
-                                      //variavel Login*/
   
- //Se voltar alguma coisa vai ser feito o select da senha
+        if($query->rowCount() != 0){
 
-        //Manipulação de informações que foram retirados do banco
-        foreach($query->fetchAll() as $valor){
-      
-        session_start();
 
-        $_SESSION['nome'] =               $valor['nome'];
-        $_SESSION['email'] =             $valor['email'];
-        $_SESSION['informacoes'] = $valor['informacoes'];
-        $_SESSION['cpf'] =                 $valor['cpf'];
-        $_SESSION['jogo'] =       $valor['jogofavorito'];
-        $_SESSION["imagem"] =        $valor["diretorio"];
+          $query = $con->prepare("SELECT * FROM usuarios WHERE cpf = :cpf");
+          $query->bindValue(":cpf", $cpf);
+          $query->execute();
+          
+        }if($query->rowCount() != 0){
+            //Manipulação de informações que foram retirados do banco
+            foreach($query->fetchAll() as $valor){
+          
 
-        /*Criação dos cookies, o que está entre aspas é o nome do cookie        
-        setcookie("nome", $Nome,               time()+3600);
-        setcookie("email", $Email,             time()+3600);
-        setcookie("informacoes", $Informacoes, time()+3600);
-        setcookie("cpf", $Cpf,                 time()+3600);
-        setcookie("jogo", $Jogo,               time()+3600);
-        */
+            $_SESSION['nome'] =               $valor['nome'];
+            $_SESSION['email'] =             $valor['email'];
+            $_SESSION['informacoes'] = $valor['informacoes'];
+            $_SESSION['cpf'] =                 $valor['cpf'];
+            $_SESSION['jogo'] =       $valor['jogofavorito'];
+            $_SESSION["imagem"] =        $valor["diretorio"];
+            
+          }
 
-        header("location: ../View/feed.php");
-  
+            header("location: ../View/feed.php");
+        }else{
 
     header("location: ../View/index.php");
-  }
+}
